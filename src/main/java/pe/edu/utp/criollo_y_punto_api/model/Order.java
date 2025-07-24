a49cc4986936d719324ac45d2bd32e5df6c77cc1
leo <leonardoelbaneado@gmail.com>
2025-07-23 19:32:10 -0500

    cont

--- src/main/java/pe/edu/utp/criollo_y_punto_api/model/Order.java
+++ src/main/java/pe/edu/utp/criollo_y_punto_api/model/Order.java
@@ -13,6 +13,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.PostLoad;
 import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
 import lombok.AllArgsConstructor;
@@ -22,7 +23,12 @@ import lombok.Setter;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import pe.edu.utp.criollo_y_punto_api.service.patron.state.CancelledOrderState;
+import pe.edu.utp.criollo_y_punto_api.service.patron.state.DeliveredOrderState;
 import pe.edu.utp.criollo_y_punto_api.service.patron.state.OrderState;
+import pe.edu.utp.criollo_y_punto_api.service.patron.state.PendingOrderState;
+import pe.edu.utp.criollo_y_punto_api.service.patron.state.PreparingOrderState;
+import pe.edu.utp.criollo_y_punto_api.service.patron.state.ReadyOrderState;
 
 @Entity
 @Table(name = "orders")
@@ -53,4 +59,60 @@ public class Order {
   @JsonIgnore
   private OrderState state;
 
+  @PostLoad
+  public void initializeState() {
+    switch (this.status.toUpperCase()) {
+      case "PENDIENTE":
+        this.state = new PendingOrderState();
+        break;
+      case "EN PREPARACION":
+        this.state = new PreparingOrderState();
+        break;
+      case "LISTO":
+        this.state = new ReadyOrderState();
+        break;
+      case "PEDIDO ENTREGADO AL CLIENTE":
+        this.state = new DeliveredOrderState();
+        break;
+      case "CANCELADO":
+        this.state = new CancelledOrderState();
+        break;
+      default:
+        this.state = new PendingOrderState();
+        break;
+    }
+  }
+
+  // Constructor con inicialización directa del estado
+  public Order(LocalDate date, LocalTime time, String tableNumber, String customerName) {
+    this.date = date;
+    this.time = time;
+    this.status = "PENDIENTE";
+    this.state = new PendingOrderState();
+  }
+
+  // Métodos de comportamiento delegados al state
+  public void startPreparation() {
+    if (state == null)
+      initializeState();
+    state.startPreparation(this);
+  }
+
+  public void markAsReady() {
+    if (state == null)
+      initializeState();
+    state.markList(this);
+  }
+
+  public void deliver() {
+    if (state == null)
+      initializeState();
+    state.deliver(this);
+  }
+
+  public void cancel() {
+    if (state == null)
+      initializeState();
+    state.cancel(this);
+  }
 }
--- src/main/java/pe/edu/utp/criollo_y_punto_api/service/OrderService.java
+++ src/main/java/pe/edu/utp/criollo_y_punto_api/service/OrderService.java
@@ -3,5 +3,11 @@ package pe.edu.utp.criollo_y_punto_api.service;
 import pe.edu.utp.criollo_y_punto_api.model.Order;
 
 public interface OrderService extends ApiService<Order> {
+    Order startPreparation(Integer orderId);
 
+    Order markAsReady(Integer orderId);
+
+    Order deliverOrder(Integer orderId);
+
+    Order cancelOrder(Integer orderId);
 }
